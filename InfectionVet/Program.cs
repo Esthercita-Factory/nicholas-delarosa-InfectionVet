3b188d51485fa4334ba52183c89bf42692f31620
nicholas-delarosa <nicholasandresdelarosarivera@hotmail.com>
2026-08-20 14:40:41 -0500

    Se cumplió con la Task 3

--- InfectionVet/Program.cs
+++ InfectionVet/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Welcome to Infection Vet!");
+﻿using InfectionVet.Models;
+
+Patient patient = new Patient
+{
+    Id = 1,
+    Name = "John Smith",
+    Age = 32,
+    Symptom = "Fever"
+};
+
+Console.WriteLine($@"Patient: {patient.Name}.
+Age: {patient.Age}.
+Symptom: {patient.Symptom}");
